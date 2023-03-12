bbd31ea2d04bf5a95c2e1f751df4d188b78bd7de
IhorProkopchuk <rectoon001@gmail.com>
2023-03-12 14:13:30 +0200

    1st

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const DELAY = 1000;
+const body = document.body;
+let intervalId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startChangeColor() {
+  startBtn.disabled = true;
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, DELAY);
+}
+
+function stopChangeColor() {
+  startBtn.disabled = false;
+  clearInterval(intervalId);
+}
+
+startBtn.addEventListener('click', startChangeColor);
+stopBtn.addEventListener('click', stopChangeColor);
